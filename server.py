62a1ef0ffb7402d352cef9bd773d3a3c09309628
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-09-22 00:26:38 +0300

    turned the project folder to a package to be able to import from the model file

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from .model import MeasurementDetector
+
 app = Flask(__name__)
 
 @app.route('/get-measurements')
 def get_measurements():
-    return 'Hello World!'
+    obj = MeasurementDetector('test_img.jpeg')
+    return obj.calculate_real_measurements()
 
 if __name__ == '__main__':
     app.run()
